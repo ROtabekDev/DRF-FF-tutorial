05a8a35f25fe5cce6428851d1efc2292bff1e0aa
Otabek <otabekrahmonovtest@gmail.com>
2022-09-29 09:53:37 +0500

    Custom User

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,55 @@
+from email.policy import default
+from enum import unique
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin
+)
 
-# Create your models here.
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, email, password=None):
+
+        if username is None:
+            raise TypeError('User da username bo`lishi kerak')
+
+        if email is None:
+            raise TypeError('User da email bo`lishi kerak')
+
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, email, password=None):
+
+        if username is None:
+            raise TypeError('User da username bo`lishi kerak')
+
+        if email is None:
+            raise TypeError('User da email bo`lishi kerak')
+
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField("Foydalanuvchi", max_length=255, unique=True, db_index=True)
+    email = models.EmailField("Email", max_length=255, unique=True, db_index=True)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def tokens(self):
+        return ''
